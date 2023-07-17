549950550db202b90fd3c935efd0c4e4c9606b1f
Christian Geraldo <geraldochristian891@gmail.com>
2023-07-17 10:02:08 +0800

    End Of adding Automapper

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,13 @@
+namespace API.DTOs
+{
+  public class PhotoDto
+  {
+    
+    public int Id { get; set; }
+
+    public string Url { get; set; }
+
+    public bool IsMain { get; set; }
+
+  }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using API.Extensions;
+using Microsoft.AspNetCore.Identity;
+
 namespace API.Entities
 {
     public class AppUser
@@ -9,5 +12,31 @@ namespace API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordHalt { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public List<Photo> Photos { get; set; } = new();
+
+        // public int GetAge(){
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
